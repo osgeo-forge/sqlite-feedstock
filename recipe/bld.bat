be9b9cf347c155743ebfdf78268c633669512b28
Larry Shaffer <lshaffer@boundlessgeo.com>
2019-01-24 03:02:49 -0700

    Fix exe symbol exports (only dll exports); add DYNAMIC_SHELL

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,11 +18,11 @@ set PLATFORM=x64
 :: -DSQLITE_ENABLE_DESERIALIZE=1
 :: -DSQLITE_ENABLE_COLUMN_METADATA=1
 
-:: Add other opts set in OSGeo4W build
-set "opts=-DSQLITE_ENABLE_FTS3_PARENTHESIS=1 -DSQLITE_ENABLE_STAT3=1 -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT=1 -D_CRT_SECURE_NO_WARNINGS=1"
+:: Add other DLL opts set in OSGeo4W build
+set "opts=-DSQLITE_ENABLE_FTS3_PARENTHESIS=1 -DSQLITE_ENABLE_STAT3=1 -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT=1"
 
-:: Note: running 'clean' target first,
-nmake /f Makefile.msc "OPTS=%opts%" clean
+:: Note: running 'clean' target first, for when debugging build
+nmake /f Makefile.msc clean
 if %errorlevel% neq 0 echo Failed to make clean & exit /b 1
 
 :: Dump version to rc file
@@ -31,8 +31,11 @@ echo VERSION = ^#define SQLITE_VERSION "%PKG_VERSION%"  1>>rcver.vc
 if %errorlevel% neq 0 echo Failed to write version to rcver.vc & exit /b 1
 
 :: Note: make all requires TCL; we only need dll and shell
-nmake /f Makefile.msc "OPTS=%opts%" core install
-if %errorlevel% neq 0 echo Failed to make core install & exit /b 1
+nmake /f Makefile.msc DYNAMIC_SHELL=1 "OPTS=%opts%" core
+if %errorlevel% neq 0 echo Failed to make core & exit /b 1
+
+nmake /f Makefile.msc install
+if %errorlevel% neq 0 echo Failed to make install & exit /b 1
 
 echo Copying sqlite3 artifacts to prefix
 copy sqlite3.exe %LIBRARY_BIN% || exit 1
